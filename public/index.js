afb736d81010a96d94ca3296dfc687b2b44b4aa9
subharthi75 <subharthi75@gmail.com>
2023-09-18 18:18:46 +0530

    header manu, responsiveness and first banner work

--- public/index.js
+++ public/index.js
@@ -1,18 +1,24 @@
 const header = document.querySelector("#header");
 const pointer_circle = document.querySelector("#pointer_circle");
+const pic_layer = document.querySelector("#first_banner #pic_layer");
 
 window.onscroll = function (e) {
   handleHeader(e);
   // handleKinet(e);
 };
 document.onload = handleHeader;
-document.addEventListener("mousemove", handleKinet);
+document.addEventListener("mousemove", function (e) {
+  handleKinet(e);
+  handlePicColorChange(detectMouseSpeed(e));
+});
 document.addEventListener("mousedown", () => {
   pointer_circle.classList.add("clicked");
+  pic_layer.querySelector("canvas").style.backgroundColor = "#000b65";
 });
 
 document.addEventListener("mouseup", () => {
   pointer_circle.classList.remove("clicked");
+  pic_layer.querySelector("canvas").style.backgroundColor = "black";
 });
 
 function handleHeader() {
@@ -25,7 +31,7 @@ function handleHeader() {
 }
 /////////////////////////////////////////////////////////////////////
 // create instance of kinet with custom settings
-var kinet = new Kinet({
+let kinet = new Kinet({
   acceleration: 0.06,
   friction: 0.2,
   names: ["x", "y"],
@@ -72,3 +78,41 @@ VANTA.TOPOLOGY({
   color: 0xed0087,
   backgroundColor: 0x000b65,
 });
+
+////////////////////////////////////////////////////////////////////////
+let timestamp = null;
+let lastMouseX = null;
+let lastMouseY = null;
+
+function detectMouseSpeed(e) {
+  if (timestamp === null) {
+    timestamp = Date.now();
+    lastMouseX = e.screenX;
+    lastMouseY = e.screenY;
+    return;
+  }
+
+  let now = Date.now();
+  let dt = now - timestamp;
+  let dx = e.screenX - lastMouseX;
+  let dy = e.screenY - lastMouseY;
+  let speedX = Math.round((dx / dt) * 100);
+  let speedY = Math.round((dy / dt) * 100);
+
+  timestamp = now;
+  lastMouseX = e.screenX;
+  lastMouseY = e.screenY;
+
+  return { speedX, speedY };
+}
+///----------------------------------------------------------------
+function handlePicColorChange(
+  { speedX = 0, speedY = 0 } = { speedX: 0, speedY: 0 }
+) {
+  let speed = Math.sqrt(speedX * speedX + speedY * speedY);
+  // console.log(speed);
+  let num = speed <= 99 ? speed : 99;
+  let col = parseInt((num / 99) * 255).toString(16);
+  // console.log(num, col);
+  pic_layer.querySelector("canvas").style.backgroundColor = "#000b65" + col;
+}
